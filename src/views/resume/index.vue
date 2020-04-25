7a60633c4e18f95d97b5645b793e18a5c62af899
murphyyi <zy84338719@hotmail.com>
2020-04-25 15:42:59 +0800

    增加信息

--- src/views/resume/index.vue
+++ src/views/resume/index.vue
@@ -16,7 +16,7 @@
                     <img alt="" class="avatar width-full rounded-2" height="200"
                          v-bind:src="user.photo" width="200">
                 </div>
-                <p><span>年龄：{{user.birthday}}
+                <p><span>年龄：{{user.birthday}} 岁
                     <br/>手机：{{user.phone}}
                     <br/>邮箱：{{user.email}}
                     <br/>户籍：{{user.native}}
@@ -51,19 +51,10 @@
                 <li id="skill9" class="s60"><span>IOS开发</span></li>
                 <li id="skill8" class="s90"><span>数据库（SQLserver、MySQL、MongoDB、Redis）</span></li>
                  -->
-                <li id="skill1" class="s70"><span>Java</span></li>
-                <li id="skill2" class="s50"><span>Spring MVC</span></li>
-                <li id="skill3" class="s40"><span>Spring Boot</span></li>
-                <li id="skill4" class="s70"><span>Linux平台使用（CentOS、Ubuntu）</span></li>
-                <li id="skill5" class="s60"><span>Python3</span></li>
-                <li id="skill6" class="s40"><span>React(ant.design) ， umi</span></li>
-                <li id="skill7" class="s60"><span>关系数据库（MySQL、MariaDB）</span></li>
-                <li id="skill8" class="s30"><span>非关系数据库（Redis、Mangodb）</span></li>
-                <li id="skill9" class="s40"><span>docker,docker-compose,k8s,docker-swarm</span></li>
+                <li v-for="(item,index) in skill" :id="`skill`+item.sequence" :class="`s`+item.percentage" :key="index">
+                    <span>{{item.name}}</span></li>
             </ul>
             <!-- end skills -->
-
-
             <div class="clear"></div>
 
             <!--EXPERIENCE TITLE-->
@@ -71,75 +62,78 @@
 
             <!--EXPERIENCE-->
             <ul id="jobs">
-                <li>
+                <li v-for="(item,index) in company" :key="index">
                     <div class="details">
-                        <h3>江苏科技大学</h3>
+                        <h3>{{item.info.company_name}}</h3>
                         <!-- <h4>江苏镇江</h4> -->
-                        <h5>2018年3月 - 2018年12月</h5>
+                        <h5>{{item.info.start}}-{{item.info.end}}</h5>
                     </div>
-                    <h3>经历</h3>
-                    <p> 参与基于Spring MVC、Mybatis3的数据可视化展现服务后端搭建。</p>
-                    <p> 数据可视化前端展现，完成H5 数据展现代码。</p>
-                    <br>
-                    <p><a>成长</a></p>
-                    <p>学会JAVA编程学习方法，将多个学科知识相互融合到一起。</p>
-                    <p>学会对需求有了思考能力。</p>
+                    <div v-for="(i,iw) in item.event" :key="iw">
+                        <h3>经历</h3>
+                        {{i.describe}}
+                        <br>
+                        <p><a>成长</a></p>
+                        {{i.harvest}}
+                        <br>
+                    </div>
                     <!-- <p>	在职期间负责mysql查表操作。</p>	 -->
                 </li>
-                <li>
-                    <div class="details">
-                        <h3>杭州大搜车汽车服务公司</h3>
-                        <!-- <h4></h4> -->
-                        <h5>2018年11月 - 2019年3月</h5>
-                    </div>
-                    <h3>经历</h3>
-                    <p>
-                        1.服务迁移——将车商服务从原来Node逻辑调用转变为新的Java服务，其中运用了ORM，Redis、MongoDB、Dobbo、zk、mq等技术，由原来串行操作转换为Java8并行流操作，优化客户体验。
-                    </p>
-                    <p>2.买手服务——服务端基于Antd构建管理页面，完成一小部分数据库表设计，开发线索查询服务，以及项目与其他子项目的数据打通。</p>
-                    <br>
-                    <p><a>成长</a></p>
-                    <p> 理解MVC思想，学会基本的Spring MVC企业级开发，对系统微服务拆分有了一点理解。</p>
-                    <p> 简单的理解react，学会基于公司后端封装的antd开发的React页面，根据后台业务，创建简单的antd业务组件。</p>
-                    <p> 了解了服务之间如何通过中间件进行服务解耦。 </p>
-                    <p> 学会代码review，学习别人的长处。</p>
-                </li>
+                <!--                <li>-->
+                <!--                    <div class="details">-->
+                <!--                        <h3>江苏科技大学</h3>-->
+                <!--                        &lt;!&ndash; <h4>江苏镇江</h4> &ndash;&gt;-->
+                <!--                        <h5>2018年3月 - 2018年12月</h5>-->
+                <!--                    </div>-->
+                <!--                    <h3>经历</h3>-->
+                <!--                    <p> 参与基于Spring MVC、Mybatis3的数据可视化展现服务后端搭建。</p>-->
+                <!--                    <p> 数据可视化前端展现，完成H5 数据展现代码。</p>-->
+                <!--                    <br>-->
+                <!--                    <p><a>成长</a></p>-->
+                <!--                    <p>学会JAVA编程学习方法，将多个学科知识相互融合到一起。</p>-->
+                <!--                    <p>学会对需求有了思考能力。</p>-->
+                <!--                    &lt;!&ndash; <p>	在职期间负责mysql查表操作。</p>	 &ndash;&gt;-->
+                <!--                </li>-->
+                <!--                <li>-->
+                <!--                    <div class="details">-->
+                <!--                        <h3>杭州大搜车汽车服务公司</h3>-->
+                <!--                        &lt;!&ndash; <h4></h4> &ndash;&gt;-->
+                <!--                        <h5>2018年11月 - 2019年3月</h5>-->
+                <!--                    </div>-->
+                <!--                    <h3>经历</h3>-->
+                <!--                    <p>-->
+                <!--                        1.服务迁移——将车商服务从原来Node逻辑调用转变为新的Java服务，其中运用了ORM，Redis、MongoDB、Dobbo、zk、mq等技术，由原来串行操作转换为Java8并行流操作，优化客户体验。-->
+                <!--                    </p>-->
+                <!--                    <p>2.买手服务——服务端基于Antd构建管理页面，完成一小部分数据库表设计，开发线索查询服务，以及项目与其他子项目的数据打通。</p>-->
+                <!--                    <br>-->
+                <!--                    <p><a>成长</a></p>-->
+                <!--                    <p> 理解MVC思想，学会基本的Spring MVC企业级开发，对系统微服务拆分有了一点理解。</p>-->
+                <!--                    <p> 简单的理解react，学会基于公司后端封装的antd开发的React页面，根据后台业务，创建简单的antd业务组件。</p>-->
+                <!--                    <p> 了解了服务之间如何通过中间件进行服务解耦。 </p>-->
+                <!--                    <p> 学会代码review，学习别人的长处。</p>-->
+                <!--                </li>-->
             </ul><!--end jobs-->
-
             <!--EDUCATION TITLE-->
             <div class="clear"></div>
             <h2 id="learn" class="sectionHead">教育</h2>
-
             <!--EDUCATION-->
             <ul id="schools">
-                <li>
+                <li v-for="(item,index) in education" :key="index">
                     <div class="details">
-                        <h3><font><font>江苏科技大学</font></font></h3>
-                        <h4><font><font>本科学历</font></font></h4>
-                        <h5><font><font>2015 - 2019年</font></font></h5>
+                        <h3><font><font>{{item.school}}</font></font></h3>
+                        <h4><font><font>{{item.formal}}</font></font></h4>
+                        <h5><font><font>{{item.start}}-{{item.end}}年</font></font></h5>
                     </div>
-                    <p><font><font>计算机科学与技术</font></font></p>
+                    <p><font><font>{{item.job}}</font></font></p>
                 </li>
             </ul><!--end schooling-->
-
             <div class="clear"></div>
-
             <!--HONORS / AWARDS TITLE-->
             <h2 id="ribbon" class="sectionHead">荣誉</h2>
-
             <!--HONORS / AWARDS-->
             <ul id="honorsAwards" style="display: block;">
-                <li><a href="
-		https://murphyyi.oss-cn-beijing.aliyuncs.com/%E7%BD%91%E6%98%93%E4%BA%91%E8%AF%BE%E5%A0%82.jpg"><font><font>网易云课堂
-                    获得python数据分析师 微专业优秀证书</font></font></a></li>
-                <li><a href="
-		https://murphyyi.oss-cn-beijing.aliyuncs.com/%E8%BE%A9%E6%89%8B.jpg"><font><font>获得
-                    计算机学院辩论比赛“最佳辩手”称号</font></font></a></li>
-                <li><a href="
-		https://murphyyi.oss-cn-beijing.aliyuncs.com/%E5%A5%96.JPG"><font><font>获得 校互联网+比赛获得校三等奖</font></font></a></li>
-
+                <li v-for="(item,index) in honor" :key="index"><a
+                        :href="item.url"><font><font>{{item.name}}</font></font></a></li>
             </ul><!--end honorsAwards-->
-
             <div class="clear"></div>
 
             <!--AS SEEN ON TITLE-->
@@ -147,55 +141,23 @@
 
             <!--AS SEEN ON-->
             <ul id="seenOn">
-                <li class="bwWrapper"><a href="game/index.html"><img
-                        src="http://murphyyi.oss-cn-beijing.aliyuncs.com/1.png" alt="超级马里奥"/></a></li>
-                <li class="bwWrapper"><a href="https://zy84338719.github.io"><img
-                        src="http://murphyyi.oss-cn-beijing.aliyuncs.com/2.png" alt="个人博客"/></a></li>
-                <li class="bwWrapper"><a href="https://www.murphyyi.com"><img
-                        src="http://murphyyi.oss-cn-beijing.aliyuncs.com/3.png" alt="个人网站"/></a></li>
-                <li class="bwWrapper"><a href="http://zy64334678.imwork.net:8888"><img
-                        src="http://murphyyi.oss-cn-beijing.aliyuncs.com/4.png" alt="docker jupyter 多语言版"/></a></li>
-                <!-- <li class="bwWrapper"><img src="assets/works/3.jpg" alt="pic3" /></li>  -->
+                <li v-for="(item,index) in works" :key="index" class="bwWrapper">
+                    <img :src="item.photo" :alt="item.name"/>
+                </li>
             </ul><!--end seenOn-->
 
             <div class="clear"></div>
 
             <!--RECOMMENDATIONS TITLE-->
             <h2 id="chat" class="sectionHead">个人评价</h2>
-
             <!--RECOMMENDATIONS-->
             <ul id="recommends" style="display: block;">
                 <p>
                     <font><font>“{{user.describe}}”</font></font>
                 </p>
             </ul>
-            <!-- <ul id="recommends" style="display: block;">
-
-                    <li><h3><font><font>客户</font></font></h3></li>
-
-                    <li>
-                        <div class="details">
-                            <h3><font><font>布朗</font></font></h3>
-                            <h4><font><font>摄影师</font></font></h4>
-                        </div>
-                        <p><font><font>“帮助我搭建了一个摄影作品合集网站，非常的完美，王小明是一个不错的开发者”</font></font></p>
-                    </li>
-                <li>
-                    <div class="details">
-                        <h3><font><font>贺幛</font></font></h3>
-                        <h4><font><font>企业家</font></font></h4>
-                    </div>
-                    <p><font><font>“帮我们设计的网站非常的不错，我们非常的满意，帮助我们提升了企业品质，有利于我们的形象宣传“</font></font></p>
-                </li>
-
-            </ul> -->
-            <!-- end recommends -->
-
             <div class="clear"></div>
-
-
         </div><!--end wrapper-->
-
         <!--COPYRIGHT-->
         <div id="copyright">&copy; 2014 - 2020 - Designed and developed by Murphyyi.</div>
 
@@ -207,7 +169,6 @@
     // import './print.css';
     // import './prettyPhoto.css'
     import {getInfoByKeyword} from '@/api/resume'
-    // import da from "element-ui/src/locale/lang/da";
     export default {
         // 数据
         data() {
@@ -229,8 +190,43 @@
                     keyword: "w",
                     watch: 20,
                     expect: "后端研发",
-                    year:8
+                    year: 8
+                },
+                education: [{
+                    school: "江苏科技大学",
+                    formal: "本科学历",
+                    start: 2015,
+                    end: 2019,
+                    job: "计算机科学与技术"
+                }],
+                works: [{
+                    name: "超级玛丽",
+                    url: "http://murphyyi.oss-cn-beijing.aliyuncs.com/1.png",
+                    photo: "http://murphyyi.oss-cn-beijing.aliyuncs.com/1.png"
                 }
+                ]
+                , skill: [
+                    {
+                        name: "",
+                        sequence: 1,
+                        percentage: 70
+                    }
+                ], company: [
+                    {
+                        info: {
+                            "code": "",
+                            "company_name": "",
+                            "company_code": ""
+                        },
+                        event: null
+                    }
+                ],honor:[
+                    {
+                        "code": "w",
+                        "name": "网易云课堂  获得python数据分析师 微专业优秀证书",
+                        "url": "https://murphyyi.oss-cn-beijing.aliyuncs.com/%E7%BD%91%E6%98%93%E4%BA%91%E8%AF%BE%E5%A0%82.jpg"
+                    },
+                ]
 
             }
         },
@@ -291,6 +287,11 @@
                 getInfoByKeyword({keyword: "w"}).then(
                     (data: object) => {
                         this.user = data.user
+                        this.works = data.works
+                        this.education = data.education
+                        this.skill = data.skill
+                        this.company = data.company
+                        this.honor = data.honor
                     }
                 )
             }
