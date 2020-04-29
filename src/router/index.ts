6da9930483bd612fbec8f0d015e00e3bf40b7813
murphyyi <zy84338719@hotmail.com>
2020-04-29 21:54:55 +0800

    更改部分样式

--- src/router/index.ts
+++ src/router/index.ts
@@ -34,6 +34,14 @@ const routes: Array<RouteConfig> = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/edit/index.vue')
+    },
+    {
+        path: '/info',
+        name: '信息',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/info/info.vue')
     }
 ]
 
